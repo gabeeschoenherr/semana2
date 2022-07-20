0d9ea9e4677b84df7f7c467bb174c16c5b533073
gabeeschoenherr <gabee.schoenherr@gmail.com>
2022-07-20 14:28:37 -0300

    Atividade 9 e 10

--- M1S02EX10/m1s02ex10.js
+++ M1S02EX10/m1s02ex10.js
@@ -1,3 +1,26 @@
 
+let i = 1;
+let nome = prompt("Digite o seu nome:");
+let array = [];
+let soma = 0;
 
+while (i!=5){
+    let num = Number( prompt(`Digite a nota ${i}`));
+    array.push(num);
 
+i++;
+}
+
+for (let a = 0; a < array.length; a++) {
+    soma += array[a];
+}
+
+let somaTotal = soma / 4;
+
+if (somaTotal > 7){
+    alert(`O aluno ${nome} tirou a média ${somaTotal} e foi aprovado.`);
+}
+else{
+    alert(`O aluno ${nome} tirou a média ${somaTotal} e foi reprovado.`);
+
+}
--- M1S02EX9/m1s02ex9.js
+++ M1S02EX9/m1s02ex9.js
@@ -2,8 +2,9 @@
 
 var produto;
 var quantProduto;
-var quantProdutoMaior;
+var quantProdutoMaior = 0;
 var i=0;
+var produtoMaior;
 
 
 while (i!=6){
@@ -12,35 +13,37 @@ var num = Number( prompt("Escolha sua opção: (1)Hortifruti (2)Laticínios (3)C
 switch (num) {
   case 1:
     produto = "Hortifruti";
-    quantProduto = Number( prompt("Escolha a quantidade de itens:"));
+    quant(quantProduto);
     break;
   case 2:
     produto = "Laticínios";
-    quantProduto = Number( prompt("Escolha a quantidade de itens:"));
+    quant(quantProduto);
     break;
   case 3:
     produto = "Carnes";
-    quantProduto = Number( prompt("Escolha a quantidade de itens:"));
+    quant(quantProduto);
     break;
   case 4:
     produto = "Peixes";
-    quantProduto = Number( prompt("Escolha a quantidade de itens:"));
+    quant(quantProduto);
     break;
   case 5:
     produto = "Aves";
-    quantProduto = Number( prompt("Escolha a quantidade de itens:"));
+    quant(quantProduto);
     break;
   case 6:
     i=6;
-    document.write(produto," ", quantProdutoMaior); 
+    document.write(produtoMaior," ", quantProdutoMaior); 
     break;   
  }
  
  }
 
  function quant(quantProduto) {
+  quantProduto = Number( prompt("Escolha a quantidade de itens:"));
   if(quantProdutoMaior<quantProduto){
     quantProdutoMaior = quantProduto;
+    produtoMaior = produto;
   }
 }
   
